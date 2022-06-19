6960eb14223e4fa7032c0129d8dd8896cc7e277b
towers92 <tokimek@gmail.com>
2022-06-18 19:24:05 -0500

    api en servidor

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import store from './store'
 import axios from 'axios'
 import './registerServiceWorker'
 
-axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.baseURL = 'https://utsc10a.pythonanywhere.com/'
 
 createApp(App).use(store).use(router, axios).mount('#app')
